71730d5bf9453b68c6c9ce737665cfd84e1ea646
Jallen Messersmith <jallenmessersmith@gmail.com>
2019-08-09 21:46:15 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,17 +1,21 @@
 def addition(num1, num2)
-
+  add = num1 + num2
+  add
 end
 
 def subtraction(num1, num2)
-
+  subt = num2 - num1
+  subt 
 end
 
 def division(num1, num2)
-
+  divs = num2 / num1
+  divs
 end
 
 def multiplication(num1, num2)
-
+  mult = num1 * num2
+  mult 
 end
 
 def modulo(num1, num2)
